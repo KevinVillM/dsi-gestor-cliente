3685f961af3a0888e7f2bfbed6fb353687a8071a
KevinVillM <89365161+KevinVillM@users.noreply.github.com>
2023-04-25 22:38:55 -0600

    Primer commit

---
+++ src/Dsi-app-client.jsx
@@ -0,0 +1,12 @@
+
+export const App = () => {
+    return (
+        <div>
+            <h1>DSI-Frontend</h1>
+            <p>DSI-Frontend is a React app that is rendered on the client side.</p>
+            <p>It is a sibling of DSI-Backend, which is a Node.js app that is rendered on the server side.</p>
+            <p>DSI-Frontend and DSI-Backend are both part of the DSI-App.</p>
+        </div>
+    );
+};
+
