190ca4af8199480352bad5aac5559ffda9765aba
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2023-04-27 16:10:49 -0600

    Añadido login (parcial)
    Adicion de bootstrap ref en el index y pagina de inicio de sesión

--- src/Dsi-app-client.jsx
+++ src/Dsi-app-client.jsx
@@ -1,12 +1,12 @@
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Login from "./pages/Autenticación/Login.jsx";
 export const App = () => {
     return (
-        <div>
-            <h1>DSI-Frontend</h1>
-            <p>DSI-Frontend is a React app that is rendered on the client side.</p>
-            <p>It is a sibling of DSI-Backend, which is a Node.js app that is rendered on the server side.</p>
-            <p>DSI-Frontend and DSI-Backend are both part of the DSI-App.</p>
-        </div>
+      <BrowserRouter>
+        <Routes>
+            <Route  path = "/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     );
 };
 
