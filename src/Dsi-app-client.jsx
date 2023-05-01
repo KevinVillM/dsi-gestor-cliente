f858bbbee1a28d89a3b364b0a7fda73d3ff5f87a
kat7058 <katerinag17019@gmail.com>
2023-05-01 00:54:55 -0600

    Vista Proyectos

--- src/Dsi-app-client.jsx
+++ src/Dsi-app-client.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Login from "./pages/Autenticación/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import Proyectos from "./pages/Proyectos.jsx";
 export const App = () => {
     return (
       <BrowserRouter>
         <Routes>
             <Route  path = "/login" element={<Login/>}/>
-            <Route path={"/dashboard"} element={<Dashboard />} />
+            <Route path={"/dashboard"} element={<Dashboard />}/>
+            <Route path = { "/proyectos"} element={<Proyectos/>}/>
         </Routes>
       </BrowserRouter>
     );
