a2a5a7c708726c77f57be3c85bf811a9be598446
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2023-06-04 13:39:01 -0600

    Update Perfil.jsx

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,30 +1,74 @@
-import react from 'react'
-import Container from "@mui/material/Container";
-import {Grid} from "@mui/material";
+import react, {useEffect, useState} from 'react'
+
 import Avatar from "@mui/material/Avatar";
+import {Skeleton} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 
 function Perfil(){
+    let [informacionPersonal,setInformacionPersonal] = useState();
+
+    let headers = new Headers
+
+    headers.append("x-token", sessionStorage.getItem("token"));
+    headers.append("Content-Type", "application/json");
+
+
+    useEffect ( () => {
+        fetch( 'http://localhost:8080/api/usuarios/'+localStorage.getItem('uid'),{
+            method: 'get',
+            headers:headers,
+        }).then( responseRaw => responseRaw.json())
+            .then(response => {
+                console.log(response.usuario)
+                setInformacionPersonal(response.usuario)
+            } )
+
+    },[])
 
 
     return <>
-    <Container>
-        <Grid container >
+        <div>
 
-            <Grid xs={8}>
-                <Avatar/>
-            </Grid>
-            <Grid xs={4}>
-                <Typography>
-                    Nombre completo
-                </Typography>
-            </Grid>
+            <div className={'container'}>
+                <div className={'row'}>
+                    <div className={'col-md mb-4 mt-2'}>
+                        <div className={'d-flex justify-content-center'}>
+                            {
+                                informacionPersonal ?
+                                    <img alt={'Foto de perfil'} />
+                                    :
+                                    <Skeleton variant={'circular'}>
+                                        <Avatar sx={{ width: 150, height: 150 }} />
+                                    </Skeleton>
 
-        </Grid>
-    </Container>
+                            }
+                        </div>
+
+                    </div>
+                    <div className={'col mb-4 mt-4'}>
+                        <Typography variant={'h5'}>
+                            {
+                                informacionPersonal ?
+                                    'Nombre: '+ informacionPersonal.nombre
+                                    :
+                                    <Skeleton/>
+                            }
+                        </Typography>
+
+                        <Typography variant={'h5'}>
+                            {
+                                informacionPersonal ?
+                                    'Correo electronico: '+informacionPersonal.email
+                                    :
+                                    <Skeleton/>
+                            }
+                        </Typography>
+                </div>
+            </div>
+            </div>
+        </div>
     </>
-
 }
 
 export default Perfil;
