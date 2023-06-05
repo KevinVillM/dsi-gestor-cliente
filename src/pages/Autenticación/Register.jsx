0a51113f2e0f2787c1e0ab70d1297ce56f39c9e1
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2023-06-05 13:37:38 -0600

    Creacion de user parcial
    Implementacion parcial de la creacion de un usuario

--- src/pages/Autenticación/Register.jsx
+++ src/pages/Autenticación/Register.jsx
@@ -1,10 +1,91 @@
-import react from 'react'
+import react, {useState} from 'react'
 import {TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import {Image} from "@mui/icons-material";
 
 
 function Registro(){
+
+    let [nombre,setNombre] = useState();
+    let [password,setPassword] = useState();
+    let [passwordConfirm,setPasswordConfirm] = useState();
+    let [email,setEmail] = useState();
+
+    //Errores
+    let [errorPasswordTooShort,setErrorPasswordTooShort] = useState(false)
+    let [errorPassConfirmTooShort,setErrorPassConfirmTooShort] = useState(false)
+    let [errorNonMatchingPasswords,setErrorNonMatchingPasswords] = useState(false)
+    let [errorEmailFormat,setErrorEmailFormat] = useState(false)
+
+
+    //Misc
+    let [progressBarActive,setProgressBarActive] = useState(false)
+
+    const changeNombre  = e => {
+        setNombre(e.target.value)
+    }
+
+    const changePassword = e =>{
+        if(e.validity.tooShort){
+            setErrorPasswordTooShort(true)
+            return;
+        }
+
+        setErrorPasswordTooShort(false)
+        setPassword(e.target.value)
+    }
+
+    const changePasswordConfirm = e => {
+
+        if (e.validity.tooShort === true){
+            setErrorPasswordTooShort(true)
+            return;
+        }
+
+        setErrorPassConfirmTooShort(false)
+        setPasswordConfirm(e.target.value)
+    }
+
+    const changeEmail = e => {
+        if(e.validity.typeMismatch){
+            setErrorEmailFormat(true)
+            return;
+        }
+        setErrorEmailFormat(false)
+        setEmail(e.target.value)
+    }
+
+
+    const crearCuenta = () => {
+
+        const headers = new Headers
+        const usuario = {
+            nombre:nombre,
+            password:password,
+            email:email,
+            img:'',
+            rol:'',
+            estado:true,
+            google:false
+        }
+
+        headers.set('Content-Type','application/json')
+
+        const init = {
+            method:'Post',
+            headers:headers,
+            body:JSON.stringify(usuario)
+        }
+
+        fetch('http://localhost:8080/api/usuarios',init)
+            .then(response => {
+                if(response.ok){
+                // TO-DO*** Mostrar un cuadro de dialogo indicando que se creo el usuario y luego redirigir al dashboard
+                }
+            })
+
+
+    }
 
 
     return <>
@@ -17,7 +98,12 @@ function Registro(){
                 <label className={'mb-4'}>Nombre completo</label>
                 <TextField className={'form-control form-control-sm mb-4'}/>
                 <label className={'mb-4'}>Correo electronico</label>
-                <TextField className={'form-control form-control-sm'}/>
+                <TextField className={'form-control form-control-sm'}
+                           type={'email'}
+                           error={errorEmailFormat}
+                           helperText={'Introduzca un email valido.'}
+                           onChange={changeEmail}
+                           required/>
             </div>
 
             <div className={'col-sm d-flex justify-content-center'}>
@@ -34,11 +120,30 @@ function Registro(){
             <div className={'row mb-4'} style={{paddingRight:0}} >
                 <div className={'col-sm-6'} style={{paddingRight:0}}>
                     <label>Contraseña</label>
-                    <TextField className={'form-control'} type={'password'}/>
+                    <TextField className={'form-control'}
+                               type={'password'}
+                               required
+                               error={errorPasswordTooShort | errorNonMatchingPasswords}
+                               helperText={errorPassConfirmTooShort ?
+                                   'La contraseña debe tener almenos 6 caracteres'
+                                   :'Las contraseñas no coinciden.'
+                               }
+                               onChange={changePassword}
+                               inputProps={{minLength:6}}/>
                 </div>
+
                 <div className={'col-sm-6'} style={{paddingRight:0}}>
                     <label>Confirmar contraseña</label>
-                    <TextField className={'form-control'} type={'password'}/>
+                    <TextField className={'form-control'}
+                               type={'password'}
+                               required
+                               error={errorPassConfirmTooShort | errorNonMatchingPasswords}
+                               helperText={errorPassConfirmTooShort ?
+                                   'La contraseña debe tener almenos 6 caracteres'
+                                   :'Las contraseñas no coinciden.'
+                               }
+                               onChange={changePasswordConfirm}
+                               inputProps={{minLenght:6}}/>
                 </div>
             </div>
 
@@ -46,10 +151,11 @@ function Registro(){
 
         </div>
         <div className={'row text-center'}>
-            <div className={'col'}>
-                <a type={'button'} className={'btn btn-primary'}>Crear cuenta.</a>
 
+            <div className={'col'}>
+                <a type={'button'} className={'btn btn-primary'} onClick={}>Crear cuenta.</a>
             </div>
+
             <div className={'col'}>
                 <a type={'button'} className={'btn'} style={{backgroundColor:'#2F58CD',color:'white'}}>Iniciar sesión.</a>
             </div>
