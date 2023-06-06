197005615141f2ee530d4e38c9f54adec17ff716
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2023-06-06 09:49:38 -0600

    Update ResponsiveAppBar.jsx

--- src/Componentes/ResponsiveAppBar.jsx
+++ src/Componentes/ResponsiveAppBar.jsx
@@ -69,7 +69,7 @@ function ResponsiveAppBar(props) {
                             variant="h6"
                             noWrap
                             component="a"
-                            href="/"
+                            href="/dashboard"
                             sx={{
                                 mr: 2,
                                 display: { xs: 'none', md: 'flex' },
