737c3703e247e084085fc9e581857fed709f0d11
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2023-09-02 22:14:48 -0600

    fixed responsiveness

--- src/Componentes/PerfilCardBasic.jsx
+++ src/Componentes/PerfilCardBasic.jsx
@@ -3,6 +3,7 @@ import {Card, CardActions, CardContent, CardHeader, Grid, TextField} from "@mui/
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
 
 
 export default function PerfilCardBasic(info){
@@ -13,32 +14,31 @@ export default function PerfilCardBasic(info){
                 </CardHeader>
 
                 <CardContent>
-                    <Grid container>
-                        <Grid item sm={12} md={5}>
-                           <img src={'https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg'} width={100} height={100}/>
-                        </Grid>
-
-                        <Grid item >
-                            <Grid container sm={12} md={7}>
-                                <Grid item xs={12}>
-                                    <Typography variant={'h6'}>Nombre</Typography>
-                                    <TextField></TextField>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Typography variant={'h6'}>Correo electronico</Typography>
-                                    <TextField contentEditable={false}></TextField>
-                                </Grid>
+                    <Grid container rowSpacing={5}>
+                        <Grid item container justifyContent={'center'} sm={12} md={6}>
+                            <Grid item>
+                                <img alt={'perfil'}/>
                             </Grid>
                         </Grid>
+
+                        <Grid item sm={12} md={6}>
+                            <Typography variant={'h5'}>Nombre</Typography>
+                        </Grid>
+
+                        <Grid item sm={12} md={12} xs={12}>
+                                <Stack alignItems={'flex-start'} spacing={2}>
+                                    <Typography variant={'h5'}>Seguridad.</Typography>
+                                    <Button>Cambiar contraseña.</Button>
+                                    <Button>Restablecer contraseña.</Button>
+                                </Stack>
+                        </Grid>
                     </Grid>
 
-                    <Grid item>
-                        <Stack alignItems={'flex-start'} spacing={2}>
-                            <Typography variant={'h5'}>Seguridad.</Typography>
-                            <Button>Cambiar contraseña.</Button>
-                            <Button>Restablecer contraseña.</Button>
-                        </Stack>
-                    </Grid>
+
+
+
+
+
                 </CardContent>
 
                 <CardActions>
