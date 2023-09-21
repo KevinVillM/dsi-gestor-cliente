829b0f94f87f7266cddadb7a148258b71a34e86b
kat7058 <katerinag17019@gmail.com>
2023-09-20 19:38:11 -0600

    actualizando footer

--- src/pages/Footer/footer.jsx
+++ src/pages/Footer/footer.jsx
@@ -1,71 +1,86 @@
 import React from "react";
 
-export default function Footer(){
-    const footerStyle = {
-        backgroundColor: "#f1f1f1",
-      };
-    
-      return (
-        <div className="container my-5">
-          <footer className="text-center text-white " style={footerStyle}>
-            <div className="container pt-4">
-  
-    <section className=" container absolute">
-   
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i className="fab fa-facebook-f"></i></a>
+export default function Footer() {
+  const footerStyle = {
+    backgroundColor: "#f1f1f1",
+  };
 
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i className="fab fa-twitter"></i></a>
+  return (
+    <footer className="text-center text-white" style={footerStyle}>
+      <div className="container-fluid pt-4">
+        <section className="row">
+          <div className="col">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fab fa-google"></i>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </section>
+      </div>
 
-     
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i className="fab fa-google"></i></a>
-
-      
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i className="fab fa-instagram"></i></a>
-
-
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i className="fab fa-linkedin"></i></a>
-      <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i className="fab fa-github"></i></a>
-    </section>
- 
-  </div>
- 
-
-  <div className="text-center text-white p-5   absolute" style={{ backgroundColor: "#214A87" }}>
-    © 2020 Copyright:
-    <a className="text-white" href="https://mdbootstrap.com/">SGDP.com</a>
-  </div>
-</footer>
-  
-</div>
-    )
+      <div
+        className="text-center text-white p-5"
+        style={{ backgroundColor: "#214A87" }}
+      >
+        © 2023 Copyright:
+        <a className="text-white" href="#!">
+          SGDP.com
+        </a>
+      </div>
+    </footer>
+  );
 }
